bb129a81b32ecd0f8a98c760d277c05a0dbfbf2b
Carl <carlctha1@gmail.com>
2023-11-21 23:08:42 +0100

    Implemented background

--- main.py
+++ main.py
@@ -2,22 +2,39 @@ import os
 import random
 import math
 import pygame
-
 from os import listdir
 from os.path import isfile, join
 pygame.init()
 
 pygame.display.set_caption("Platformer")
 
-BACKGROUND = (255, 255, 255)
 WIDTH, HEIGHT = 600, 600
 FPS = 60
 PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def create_background(name):
+    img = pygame.image.load(join("assets", "background", name))
+    _, _, width, height = img.get_rect()
+    tiles = []
+
+    for x_tile in range(WIDTH // width + 1):
+        for y_tile in range(HEIGHT // height + 1):
+            position = (x_tile * width, y_tile * height)
+            tiles.append(position)
+
+    return tiles, img
+
+def draw_img(window, background, background_img):
+    for tile in background:
+        window.blit(background_img, tile)
+
+    pygame.display.update()
+
 def main(window):
     clock = pygame.time.Clock()
+    background, background_img = create_background("Purple.png")
 
     is_running = True
     while is_running:
@@ -27,6 +44,8 @@ def main(window):
             if event.type == pygame.QUIT:
                 is_running = False
                 break
+        
+        draw_img(window, background, background_img)
     
     pygame.quit()
     quit()
